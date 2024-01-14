1cf97ef886dbfaba2299f99031e0daf7749d1006
LucDeCaf <ldecafmeyer@gmail.com>
2024-01-14 19:56:32 +0200

    Extracted state update handler into its own function for reuse later

--- src/components/views/card-view.tsx
+++ src/components/views/card-view.tsx
@@ -4,8 +4,9 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
 import type { Quota } from "@/App";
+import { handleInputFieldChange } from "@/lib/utils";
 
-export interface InputFieldProps {
+export interface CardInputFieldProps {
     quota: Quota;
     field: "day1" | "day2" | "day3" | "sold";
     setter: React.Dispatch<React.SetStateAction<Quota[]>>;
@@ -16,7 +17,7 @@ export interface CardViewProps {
     setter: React.Dispatch<React.SetStateAction<Quota[]>>;
 }
 
-export function InputField({ quota, field, setter }: InputFieldProps) {
+export function CardInputField({ quota, field, setter }: CardInputFieldProps) {
     return (
         <div className="flex flex-col gap-4">
             <Label htmlFor={`${quota.uid}-${field}`}>Day 1</Label>
@@ -25,25 +26,7 @@ export function InputField({ quota, field, setter }: InputFieldProps) {
                 type="number"
                 value={quota[field]}
                 onChange={(e) =>
-                    setter((prev) =>
-                        prev.map((q) => {
-                            switch (q.uid) {
-                                case quota.uid:
-                                    let value = e.target.value;
-
-                                    return {
-                                        ...quota,
-                                        [field]:
-                                            value == ""
-                                                ? 0
-                                                : Number.parseInt(value),
-                                    };
-                                    break;
-                                default:
-                                    return q;
-                            }
-                        })
-                    )
+                    handleInputFieldChange(e, quota, field, setter)
                 }
             />
         </div>
@@ -70,22 +53,22 @@ export function CardView({ quotas, setter }: CardViewProps) {
                         </Button>
                     </CardHeader>
                     <CardContent className="flex flex-col gap-4">
-                        <InputField
+                        <CardInputField
                             quota={quota}
                             field="day1"
                             setter={setter}
                         />
-                        <InputField
+                        <CardInputField
                             quota={quota}
                             field="day2"
                             setter={setter}
                         />
-                        <InputField
+                        <CardInputField
                             quota={quota}
                             field="day3"
                             setter={setter}
                         />
-                        <InputField
+                        <CardInputField
                             quota={quota}
                             field="sold"
                             setter={setter}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,31 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Quota } from "@/App";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function handleInputFieldChange(
+    event: React.ChangeEvent<HTMLInputElement>,
+    quota: Quota,
+    field: "day1" | "day2" | "day3" | "sold",
+    setter: React.Dispatch<React.SetStateAction<Quota[]>>
+) {
+    setter((prev) =>
+        prev.map((q) => {
+            switch (q.uid) {
+                case quota.uid:
+                    let value = event.target.value;
+
+                    return {
+                        ...quota,
+                        [field]: value == "" ? 0 : Number.parseInt(value),
+                    };
+                    break;
+                default:
+                    return q;
+            }
+        })
+    );
 }
