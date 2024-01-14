13ebe945cedbae7aab0e416e4288b0a2510697fe
LucDeCaf <ldecafmeyer@gmail.com>
2024-01-14 12:16:34 +0200

    Fixed many bugs + added totals + added deletion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import { nanoid } from "nanoid";
 
 type QuotaType = {
-    id: number;
+    uid: string;
     day1: number;
     day2: number;
     day3: number;
@@ -15,60 +18,157 @@ export default function App() {
 
     let totalSold = 0;
     let totalOnShip = 0;
-
+    console.log(quotas);
     quotas.forEach((quota) => {
         totalSold += quota.sold;
         totalOnShip += quota.day1 + quota.day2 + quota.day3 - quota.sold;
     });
 
-    let lastId = quotas.length;
-
     const addQuota = () => {
-        lastId++;
         setQuotas((prev) => [
-            ...prev,
             {
-                id: lastId,
+                uid: nanoid(),
                 day1: 0,
                 day2: 0,
                 day3: 0,
                 sold: 0,
             },
+            ...prev,
         ]);
     };
 
-    const updateQuota = (id: number, state: QuotaType) => {
+    const updateQuota = (uid: string, state: QuotaType) => {
         setQuotas((prev) =>
-            prev.map((quota) => (quota.id == id ? state : quota))
+            prev.map((quota) => (quota.uid == uid ? state : quota))
         );
     };
 
-    let quotaElements = quotas.map((quota) => (
-        <Card>
-            <CardHeader>
-                <CardTitle>Quota {quota.id}</CardTitle>
+    let quotaElements = quotas.map((quota, index) => (
+        <Card key={quota.uid}>
+            <CardHeader className="flex flex-row items-center justify-between">
+                <CardTitle>Quota {quotas.length - index}</CardTitle>
+                <Button
+                    variant="outline"
+                    onClick={() =>
+                        setQuotas((prev) =>
+                            prev.filter((q) => q.uid != quota.uid)
+                        )
+                    }
+                >
+                    X
+                </Button>
             </CardHeader>
-            <CardContent>
-                
+            <CardContent className="flex flex-col gap-4">
+                <Label htmlFor={`${quota.uid}-day1`}>Day 1</Label>
+                <Input
+                    id={`${quota.uid}-day1`}
+                    type="number"
+                    onChange={(e) =>
+                        setQuotas((prev) =>
+                            prev.map((q) =>
+                                q.uid == quota.uid
+                                    ? {
+                                          ...quota,
+                                          day1: Number.parseFloat(
+                                              e.target.value
+                                          ),
+                                      }
+                                    : q
+                            )
+                        )
+                    }
+                />
+                <Label htmlFor={`${quota.uid}-day2`}>Day 2</Label>
+                <Input
+                    id={`${quota.uid}-day2`}
+                    type="number"
+                    onChange={(e) =>
+                        setQuotas((prev) =>
+                            prev.map((q) =>
+                                q.uid == quota.uid
+                                    ? {
+                                          ...quota,
+                                          day2: Number.parseFloat(
+                                              e.target.value
+                                          ),
+                                      }
+                                    : q
+                            )
+                        )
+                    }
+                />
+                <Label htmlFor={`${quota.uid}-day3`}>Day 3</Label>
+                <Input
+                    id={`${quota.uid}-day3`}
+                    type="number"
+                    onChange={(e) =>
+                        setQuotas((prev) =>
+                            prev.map((q) =>
+                                q.uid == quota.uid
+                                    ? {
+                                          ...quota,
+                                          day3: Number.parseFloat(
+                                              e.target.value
+                                          ),
+                                      }
+                                    : q
+                            )
+                        )
+                    }
+                />
+                <Label htmlFor={`${quota.uid}-sold`}>Sold</Label>
+                <Input
+                    id={`${quota.uid}-sold`}
+                    type="number"
+                    onChange={(e) =>
+                        setQuotas((prev) =>
+                            prev.map((q) =>
+                                q.uid == quota.uid
+                                    ? {
+                                          ...quota,
+                                          day4: Number.parseFloat(
+                                              e.target.value
+                                          ),
+                                      }
+                                    : q
+                            )
+                        )
+                    }
+                />
             </CardContent>
         </Card>
     ));
 
     return (
         <main className="flex flex-col gap-6 p-8 pt-16 text-white">
-            <h1 className="font-mono text-4xl">Quota Tracker</h1>
+            <div className="flex justify-between">
+                <h1 className="font-mono text-4xl">Quota Tracker</h1>
+                <div className="flex gap-2 font-mono">
+                    <Button variant="link">Options</Button>
+                    <Button variant="outline" onClick={() => setQuotas([])}>
+                        Clear
+                    </Button>
+                </div>
+            </div>
 
-            <Button variant="outline" className="font-mono text-3xl" onClick={addQuota}>
+            <div className="font-mono text-2xl">
+                <p>Total On Ship: {totalOnShip}</p>
+                <p>Total Sold: &nbsp;&nbsp;&nbsp;{totalSold}</p>
+            </div>
+
+            <Button
+                variant="outline"
+                className="font-mono text-3xl"
+                onClick={addQuota}
+            >
                 Add Quota
             </Button>
 
-            {quotas.length > 0 ? (
+            {quotas.length > 0 && (
                 <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
                     {quotaElements}
                 </div>
-            ) : (
-                <h2></h2>
-            )}            
+            )}
         </main>
     );
 }
