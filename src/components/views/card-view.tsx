a8394d148a19553e35115de3eecfee0e89f4c75d
LucDeCaf <ldecafmeyer@gmail.com>
2024-01-16 09:27:31 +0200

    Added net profit to card view

--- src/components/views/card-view.tsx
+++ src/components/views/card-view.tsx
@@ -4,15 +4,22 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
 import { VALID_FIELDS, type Quota } from "@/App";
-import type { ViewProps } from "@/lib/utils";
+import type { Setter, ViewProps } from "@/lib/utils";
 import { handleInputFieldChange } from "@/lib/utils";
 import { Options } from "../options";
 
-export interface CardInputFieldProps {
+interface CardInputFieldProps {
     quota: Quota;
     options: Options;
     field: "day1" | "day2" | "day3" | "sold";
-    setter: React.Dispatch<React.SetStateAction<Quota[]>>;
+    setter: Setter<Quota[]>;
+}
+
+interface CardItemProps {
+    quota: Quota;
+    options: Options;
+    index: number;
+    setter: Setter<Quota[]>;
 }
 
 export function CardInputField({
@@ -41,7 +48,7 @@ export function CardInputField({
     }
 
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
             <Label htmlFor={`${quota.uid}-${field}`}>{label}</Label>
             <Input
                 id={`${quota.uid}-${field}`}
@@ -56,36 +63,63 @@ export function CardInputField({
     );
 }
 
+function CardItem({ quota, index, options, setter }: CardItemProps) {
+    const netProfit = quota.day1 + quota.day2 + quota.day3 - quota.sold;
+
+    return (
+        <Card key={quota.uid}>
+            <CardHeader className="flex flex-row items-center justify-between">
+                <CardTitle>Quota {index}</CardTitle>
+                <Button
+                    variant="outline"
+                    className="w-12 h-12 p-0"
+                    onClick={() =>
+                        setter((prev) => prev.filter((q) => q.uid != quota.uid))
+                    }
+                >
+                    <X />
+                </Button>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-6">
+                <div className="flex justify-between pr-2 text-xl">
+                    <span>Net Profit:{" "}</span>
+                    {(netProfit === 0 || !options.colouredText) && (
+                        <span>{netProfit}</span>
+                    )}
+                    {options.colouredText && netProfit > 0 && (
+                        <span className="text-green-500">+{netProfit}</span>
+                    )}
+                    {options.colouredText && netProfit < 0 && (
+                        <span className="text-red-500">{netProfit}</span>
+                    )}
+                </div>
+                <hr />
+                <div className="flex flex-col gap-4">
+                    {VALID_FIELDS.map((field) => (
+                        <CardInputField
+                            quota={quota}
+                            options={options}
+                            field={field}
+                            setter={setter}
+                        />
+                    ))}
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
+
 export function CardView({ quotas, options, setter }: ViewProps) {
     return (
         <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
             {quotas.map((quota, index) => (
-                <Card key={quota.uid}>
-                    <CardHeader className="flex flex-row items-center justify-between">
-                        <CardTitle>Quota {quotas.length - index}</CardTitle>
-                        <Button
-                            variant="outline"
-                            className="w-12 h-12 p-0"
-                            onClick={() =>
-                                setter((prev) =>
-                                    prev.filter((q) => q.uid != quota.uid)
-                                )
-                            }
-                        >
-                            <X />
-                        </Button>
-                    </CardHeader>
-                    <CardContent className="flex flex-col gap-4">
-                        {VALID_FIELDS.map((field) => (
-                            <CardInputField
-                                quota={quota}
-                                options={options}
-                                field={field}
-                                setter={setter}
-                            />
-                        ))}
-                    </CardContent>
-                </Card>
+                <CardItem
+                    key={quota.uid}
+                    quota={quota}
+                    index={quotas.length - index}
+                    options={options}
+                    setter={setter}
+                />
             ))}
         </div>
     );
